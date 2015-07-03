0efcb44572e88f9cb3ae03efc7eebddb8cacf96b
Luciano Rubio <luciano@loociano.com>
2015-07-03 21:20:46 +0100

    Added functions to collect rectangles and compute levels.

---
+++ js/css-uml.js
@@ -0,0 +1,96 @@
+/**
+ * CSS-UML 
+ *
+ * Luciano Rubio <luciano@loociano.com>
+ * July 2015
+ *
+ */
+(function(){
+
+    "use strict";
+
+    var screen = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+
+    var properties = {
+        _title: "title", 
+        _extends: "extends", 
+        _type: "type"
+    };
+
+    var classes = {
+        _class: "class",
+        _abstract: "abstract"
+    };
+
+    function Rectangle(domElt){
+        this.domElt = domElt;
+        this.name = domElt.dataset.name;
+        this.refs = {
+            _extends: null // Stores reference to parent Rectangle
+        };
+        this.level = 0;
+    };
+
+    Rectangle.prototype.hasParent = function(){
+        return this.refs._extends !== null;
+    };
+
+    var classArray = document.getElementsByClassName(classes._class);
+    var rectangles = [];
+    
+    // Retrieve all Rectangles
+    for(var i = 0; i < classArray.length; i++){
+        rectangles.push( new Rectangle(classArray[i]) );
+    }
+
+    function findRectangle(name){
+        for (var r of rectangles){
+            if (r.name === name){
+                return r;
+            }
+        }
+    }
+
+    var levels = 0;
+
+    // Compute graph
+    for (var r of rectangles){
+        // Find parents
+        if (r.domElt.dataset.hasOwnProperty(properties._extends)){
+            var parent = findRectangle(r.domElt.dataset.extends);
+            if (parent !== undefined){
+                r.refs._extends = parent;
+            }
+        }
+    }
+
+    function calculateNbLevels(){
+        var levels = 0, max = 0;
+
+        for (var r of rectangles){
+            var self = r; // save reference
+            while(true){
+                var parent = r.refs._extends;
+                if (parent !== null){
+                    r = parent;
+                    levels++;
+                } else {
+                    if (levels > max){
+                        max = levels;
+                        levels = 0;
+                    }
+                    r = self;
+                    break;
+                }
+            } 
+        }
+        return max;
+    }
+     
+    console.log(calculateNbLevels());
+
+
+})();
