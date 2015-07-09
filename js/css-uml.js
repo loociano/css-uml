5f56e22b3cb17c0928dcd53852929ff95ecdbafa
Luciano Rubio <luciano@loociano.com>
2015-07-09 19:59:45 +0100

    Formatted

--- js/css-uml.js
+++ js/css-uml.js
@@ -1,96 +1,93 @@
 /**
- * CSS-UML 
- *
- * Luciano Rubio <luciano@loociano.com>
- * July 2015
- *
+ * CSS-UML
+ * 
+ * Luciano Rubio <luciano@loociano.com> July 2015
+ * 
  */
-(function(){
+(function() {
 
-    "use strict";
+  "use strict";
 
-    var screen = {
-        width: window.innerWidth,
-        height: window.innerHeight
+  var screen = {
+    width : window.innerWidth,
+    height : window.innerHeight
+  };
+
+  var properties = {
+    _title : "title",
+    _extends : "extends",
+    _type : "type"
+  };
+
+  var classes = {
+    _class : "class",
+    _abstract : "abstract"
+  };
+
+  function Rectangle(domElt) {
+    this.domElt = domElt;
+    this.name = domElt.dataset.name;
+    this.refs = {
+      _extends : null
+    // Stores reference to parent Rectangle
     };
+    this.level = 0;
+  }
 
-    var properties = {
-        _title: "title", 
-        _extends: "extends", 
-        _type: "type"
-    };
+  Rectangle.prototype.hasParent = function() {
+    return this.refs._extends !== null;
+  };
 
-    var classes = {
-        _class: "class",
-        _abstract: "abstract"
-    };
+  var classArray = document.getElementsByClassName(classes._class);
+  var rectangles = [];
 
-    function Rectangle(domElt){
-        this.domElt = domElt;
-        this.name = domElt.dataset.name;
-        this.refs = {
-            _extends: null // Stores reference to parent Rectangle
-        };
-        this.level = 0;
-    };
+  // Retrieve all Rectangles
+  for (var i = 0; i < classArray.length; i++) {
+    rectangles.push(new Rectangle(classArray[i]));
+  }
 
-    Rectangle.prototype.hasParent = function(){
-        return this.refs._extends !== null;
-    };
-
-    var classArray = document.getElementsByClassName(classes._class);
-    var rectangles = [];
-    
-    // Retrieve all Rectangles
-    for(var i = 0; i < classArray.length; i++){
-        rectangles.push( new Rectangle(classArray[i]) );
+  function findRectangle(name) {
+    for ( var index in rectangles) {
+      if (rectangles[index].name === name) {
+        return r;
+      }
     }
+  }
 
-    function findRectangle(name){
-        for (var r of rectangles){
-            if (r.name === name){
-                return r;
-            }
+  // Compute graph
+  for ( var index in rectangles) {
+    var r = rectangles[index];
+    // Find parents
+    if (r.domElt.dataset.hasOwnProperty(properties._extends)) {
+      var parent = findRectangle(r.domElt.dataset.extends);
+      if (parent !== undefined) {
+        r.refs._extends = parent;
+      }
+    }
+  }
+
+  function calculateNbLevels() {
+    var levels = 0, max = 0;
+
+    for ( var index in rectangles) {
+      var r = rectangles[index];
+      var self = r; // save reference
+      while (true) {
+        var parent = r.refs._extends;
+        if (parent !== null) {
+          r = parent;
+          levels++;
+        } else {
+          if (levels > max) {
+            max = levels;
+            levels = 0;
+          }
+          r = self;
+          break;
         }
+      }
     }
-
-    var levels = 0;
-
-    // Compute graph
-    for (var r of rectangles){
-        // Find parents
-        if (r.domElt.dataset.hasOwnProperty(properties._extends)){
-            var parent = findRectangle(r.domElt.dataset.extends);
-            if (parent !== undefined){
-                r.refs._extends = parent;
-            }
-        }
-    }
-
-    function calculateNbLevels(){
-        var levels = 0, max = 0;
-
-        for (var r of rectangles){
-            var self = r; // save reference
-            while(true){
-                var parent = r.refs._extends;
-                if (parent !== null){
-                    r = parent;
-                    levels++;
-                } else {
-                    if (levels > max){
-                        max = levels;
-                        levels = 0;
-                    }
-                    r = self;
-                    break;
-                }
-            } 
-        }
-        return max;
-    }
-     
-    console.log(calculateNbLevels());
-
+    return max;
+  }
 
 })();
